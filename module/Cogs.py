ce76c8e9b31b130fc4c2b8df4d970e4344dc2c1e
MujyKun <thugystory1@Gmail.com>
2020-01-11 22:32:30 -0500

    Create Cogs.py

---
+++ module/Cogs.py
@@ -0,0 +1,56 @@
+from discord.ext import commands
+
+client = 0
+
+
+def setup(client1):
+    client1.add_cog(Cogs(client1))
+    global client
+    client = client1
+
+
+class Cogs(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    # Hidden means it won't show up on the default help.
+    @commands.command(name='load', hidden=False)
+    @commands.is_owner()
+    async def load_cog(self, ctx, *, cog: str):
+        """Command which Loads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            self.client.load_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            await ctx.send('**`SUCCESS`**')
+
+    @commands.command(name='unload', hidden=False)
+    @commands.is_owner()
+    async def unload_cog(self, ctx, *, cog: str):
+        """Command which Unloads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            self.client.unload_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            await ctx.send('**`SUCCESS`**')
+
+
+    @commands.command(name='reload', hidden=False)
+    @commands.is_owner()
+    async def reload_cog(self, ctx, *, cog: str):
+        """Command which Reloads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            self.client.unload_extension(cog)
+            self.client.load_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            await ctx.send('**`SUCCESS`**')

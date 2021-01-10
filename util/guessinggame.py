dae14f4465faa81caf92ea7da6aa2e6cc37b54f2
A-Phamfam <alextampham3000@gmail.com>
2021-01-10 06:20:32 -0600

    removed numerical difficulty assignment

--- util/guessinggame.py
+++ util/guessinggame.py
@@ -6,22 +6,17 @@ class GuessingGame:
     async def update_user_guessing_game_score(self, difficulty, user_id, score):
         """Update a user's guessing game score."""
         try:
-            difficulty_as_string = await self.convert_difficulty_to_string(difficulty)
             user_scores = ex.cache.guessing_game_counter.get(user_id)
             # if the user does not exist, create them in the db & cache
             if not user_scores:
                 await self.create_user_in_guessing_game(user_id)
                 user_scores = {}  # set to default so getting current user score does not error.
-            difficulty_score = user_scores.get(difficulty_as_string) or 0
+            difficulty_score = user_scores.get(difficulty) or 0
             # difficulty score will always exist, no need to have a condition.
-            user_scores[difficulty_as_string] = difficulty_score + score
-            await self.update_user_score_in_db(difficulty_as_string, user_scores[difficulty_as_string], user_id)
+            user_scores[difficulty] = difficulty_score + score
+            await self.update_user_score_in_db(difficulty, user_scores[difficulty], user_id)
         except Exception as e:
             log.console(f"{e} -> update_user_guessing_game_score")
-
-    @staticmethod
-    async def convert_difficulty_to_string(difficulty_level: int):
-        return ex.cache.difficulty_levels[difficulty_level - 1]
 
     @staticmethod
     async def create_user_in_guessing_game(user_id):

7247f0607287b70703be3b6f8378f12bda74fc44
Mujy <thugystory1@gmail.com>
2021-03-17 10:05:51 -0400

    Added warning messages for developer usage on asyncpg pool

--- module/keys.py
+++ module/keys.py
@@ -141,6 +141,12 @@ postgres_options = {
 
 async def connect_to_db():
     """Create a pool to the postgres database using asyncpg"""
+    # pool is not being used as recommended, however, since we do not deal with millions of requests a second,
+    # the current usage is fine. connections from the pool are also not released after completion and we let asyncpg
+    # release the inactive connection once it recognizes it is inactive.
+    # instead of acquiring a connection from the pool, we just let the pool select a connection for us and
+    # execute directly that way. this limits the amount of methods we have access to,
+    # but in the case those methods are needed, just acquire the connection and use that instead.
     return await asyncpg.create_pool(**postgres_options, command_timeout=60)
 
 # papago translation keys - not needed - https://developers.naver.com/docs/papago/

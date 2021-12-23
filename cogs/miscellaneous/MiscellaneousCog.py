25bc6aa88065106cbc503eb41ea0485f3e8f9458
A-Phamfam <alextampham3000@gmail.com>
2021-12-23 01:26:20 -0600

    added embed pagination

--- cogs/miscellaneous/MiscellaneousCog.py
+++ cogs/miscellaneous/MiscellaneousCog.py
@@ -1,10 +1,9 @@
 from discord.ext import commands
 from dislash import slash_command, message_command, \
-    SlashInteraction, ContextMenuInteraction, OptionParam
+    SlashInteraction, ContextMenuInteraction, OptionParam, SelectMenu, SelectOption, user_command
 from random import choice, randint
 from re import findall
-from util.BotEmbed import create_bot_author_embed, add_embed_inline_fields
-
+from util.BotEmbed import create_bot_author_embed, add_embed_inline_fields, create_paginated_embed
 
 class MiscellaneousCog(commands.Cog):
     def __init__(self, bot):
@@ -83,6 +82,45 @@ class MiscellaneousCog(commands.Cog):
             pass
             # TODO: Add exception handling
 
+    @slash_command(description="Test command")
+    async def test_pages(self, inter: SlashInteraction, num_pages: int):
+        embeds = []
+        for i in range(0, num_pages):
+            embeds.append(await create_bot_author_embed(self.bot.keys, title=f"test{i + 1}"))
+
+        await create_paginated_embed(inter, embeds)
+
+    @slash_command(description="Test command")
+    async def test_menu(self, inter: SlashInteraction):
+        menu = [
+            SelectMenu(
+                custom_id="test",
+                placeholder="Choose up to 2 options",
+                max_values=2,
+                options=[
+                    SelectOption("Option 1", "value 1"),
+                    SelectOption("Option 2", "value 2"),
+                    SelectOption("Option 3", "value 3")
+                ]
+            )
+        ]
+        msg = await inter.reply("This message has a select menu!", components=menu)
+
+        def check(menu_inter):
+            nonlocal inter
+            return menu_inter.author == inter.author
+
+        menu_inter = await msg.wait_for_dropdown(check)
+        labels = [option.label for option in menu_inter.select_menu.selected_options]
+        await menu_inter.reply(f"Your choices: {', '.join(labels)}")
+
+    @user_command(name="Created at")
+    async def created_at(self, inter: ContextMenuInteraction):
+        # User commands always have only this ^ argument
+        await inter.respond(
+            f"{inter.user} was created at {inter.user.created_at}",
+            ephemeral=True # Make the message visible only to the author
+        )
 
 def setup(bot: commands.AutoShardedBot):
     bot.add_cog(MiscellaneousCog(bot))
--- util/BotEmbed.py
+++ util/BotEmbed.py
@@ -1,6 +1,7 @@
 from discord import Embed as disEmbed, User as disUser
 from random import randint
 from keys import Keys
+from dislash import ActionRow, Button, ButtonStyle, SlashInteraction
 
 
 async def create_bot_author_embed(botkeys: Keys, **kwargs) -> disEmbed:
@@ -30,7 +31,7 @@ def _get_random_color():
     return int(('%02X%02X%02X' % (r(), r(), r())), 16)  # must be specified to base 16 since 0x is not present
 
 
-async def add_embed_inline_fields(embed: disEmbed, fields: dict):
+async def add_embed_inline_fields(embed: disEmbed, fields: dict) -> disEmbed:
     """Easily add fields to an embed through a dictionary of the field names and values. {name : value}
     All fields will be inline"""
     for f_name, f_value in fields.items():
@@ -38,7 +39,7 @@ async def add_embed_inline_fields(embed: disEmbed, fields: dict):
     return embed
 
 
-async def add_embed_listed_fields(embed: disEmbed, fields: dict):
+async def add_embed_listed_fields(embed: disEmbed, fields: dict) -> disEmbed:
     """Easily add fields to an embed through a dictionary of the field names and values. {name : value}
     All fields will be not be inline."""
     for f_name, f_value in fields.items():
@@ -46,10 +47,115 @@ async def add_embed_listed_fields(embed: disEmbed, fields: dict):
     return embed
 
 
-async def set_embed_author_and_footer(botkeys: Keys, embed, footer_message):
+async def set_embed_author_and_footer(botkeys: Keys, embed, footer_message) -> disEmbed:
     """Sets the bot author and footer of an embed."""
     embed.set_author(name=botkeys.bot_name, url=botkeys.bot_website_url,
                      icon_url=botkeys.embed_icon_url)
     embed.set_footer(text=footer_message,
                      icon_url=botkeys.embed_icon_url)
     return embed
+
+
+async def get_page_buttons(num_pages: int, current_page: int) -> list[ActionRow]:
+    """Returns the 'Next' and 'Previous' buttons given the number of pages and the current page. The button will be
+    disabled if you can no longer go back or forward in pages. If there are 6 or more pages, a 'First' and 'Last'
+    button will be included."""
+    is_first = (current_page == 0)
+    is_end = (current_page == num_pages - 1)
+
+    many_page_buttons = ActionRow(
+        Button(
+            style=ButtonStyle.gray,
+            label="First",
+            custom_id="first",
+            disabled=is_first
+        ),
+        Button(
+            style=ButtonStyle.gray,
+            label="Previous",
+            custom_id="prev",
+            disabled=is_first
+        ),
+        Button(
+            style=ButtonStyle.gray,
+            label="Next",
+            custom_id="next",
+            disabled=is_end
+        ),
+        Button(
+            style=ButtonStyle.gray,
+            label="Last",
+            custom_id="last",
+            disabled=is_end
+        )
+    )
+
+    few_page_buttons = ActionRow(
+        Button(
+            style=ButtonStyle.gray,
+            label="Previous",
+            custom_id="prev",
+            disabled=is_first
+        ),
+        Button(
+            style=ButtonStyle.gray,
+            label="Next",
+            custom_id="next",
+            disabled=is_end
+        )
+    )
+    if num_pages >= 6:
+        return [many_page_buttons]
+    else:
+        return [few_page_buttons]
+
+
+async def create_paginated_embed(inter: SlashInteraction, embeds: list[disEmbed]) -> disEmbed:
+    """Create a paginated embed with Previous/Next buttons to navigate through the different pages."""
+    page_buttons = await get_page_buttons(len(embeds), 0)
+
+    embed_idx = 0
+    msg = await inter.reply(embed=embeds[embed_idx], components=page_buttons)
+    on_click = msg.create_click_listener(timeout=60)
+
+    @on_click.matching_id("first")
+    async def first_button(inter: SlashInteraction):
+        nonlocal embed_idx, msg, embeds
+        embed_idx = 0
+
+        new_page_buttons = await get_page_buttons(len(embeds), embed_idx)
+        await msg.edit(embed=embeds[embed_idx], components=new_page_buttons)
+        await inter.acknowledge()
+
+    @on_click.matching_id("prev")
+    async def prev_button(inter: SlashInteraction):
+        nonlocal embed_idx, msg, embeds
+        embed_idx -= 1
+
+        new_page_buttons = await get_page_buttons(len(embeds), embed_idx)
+        await msg.edit(embed=embeds[embed_idx], components=new_page_buttons)
+        await inter.acknowledge()
+
+    @on_click.matching_id("next")
+    async def next_button(inter: SlashInteraction):
+        nonlocal embed_idx, msg, embeds
+        embed_idx += 1
+
+        new_page_buttons = await get_page_buttons(len(embeds), embed_idx)
+        await msg.edit(embed=embeds[embed_idx], components=new_page_buttons)
+        await inter.acknowledge()
+
+    @on_click.matching_id("last")
+    async def last_button(inter: SlashInteraction):
+        nonlocal embed_idx, msg, embeds
+        embed_idx = len(embeds) - 1
+
+        new_page_buttons = await get_page_buttons(len(embeds), embed_idx)
+        await msg.edit(embed=embeds[embed_idx], components=new_page_buttons)
+        await inter.acknowledge()
+
+    @on_click.timeout
+    async def on_timeout():
+        nonlocal msg
+        await msg.edit(components=[])
+

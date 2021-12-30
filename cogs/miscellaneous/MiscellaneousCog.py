b6a8bbdbdc7e2a7315bd00f55a17ded93a212832
A-Phamfam <alextampham3000@gmail.com>
2021-12-30 13:04:15 -0600

    added choose and banner bot commands

--- cogs/miscellaneous/MiscellaneousCog.py
+++ cogs/miscellaneous/MiscellaneousCog.py
@@ -10,32 +10,33 @@ class MiscellaneousCog(commands.Cog):
 
     @commands.slash_command(name="8ball", description="Ask the 8ball a question.")
     async def _8ball(self, inter: AppCmdInter,
-                     question: str = commands.Param("Question to ask the 8ball.")):
+                     question: str = commands.Param(description="Question to ask the 8ball.")):
         await inter.send(f"**Question**: {question}\n" + f"**Answer**: Not implemented yet.")
         # TODO: add 8ball responses from the database
 
+    @commands.slash_command(description="Choose between a selection of options.")
+    async def choose(self,
+                     inter: AppCmdInter,
+                     choices: str = commands.Param(description="List the different options with `|` between choices")):
+        random_selection = choice(choices.split("|"))
+        possible_choices = choices.split("|")
+        possible_choices = str.join(", ", ["`" + my_choice + "`" for my_choice in possible_choices])
+        await inter.send(f"**Possible Choices**: {possible_choices}\n" + f"**Selection**: `{random_selection}`")
+
     @commands.slash_command(description="Display Emoji")
-    async def displayemoji(self, inter: AppCmdInter, emoji_content: str = commands.Param(description="Emoji to be displayed")):
+    async def displayemoji(self,
+                           inter: AppCmdInter,
+                           emoji_content: str = commands.Param(description="Emoji to be displayed")):
         if not emoji_content:
             await inter.send("No emote detected.", ephemeral=True)
             return
-        emoji_info = findall(r"<(a?):?\w+:([0-9]{18})>", emoji_content)
-        if not emoji_info:
+        emoji_infos = findall(r"<a?:?\w+:[0-9]{18}>", emoji_content)
+        if not emoji_infos:
             await inter.send("No emote content detected. It may be a default system emote.", ephemeral=True)
             return
-        emoji_urls = [f"https://cdn.discordapp.com/emojis/{emoji_id}.{'gif' if isAnimated else 'png'}" for
-                      isAnimated, emoji_id in emoji_info]
+        emoji_urls = [disnake.PartialEmoji.from_str(emoji_info).url for emoji_info in emoji_infos]
         await inter.send("\n".join(emoji_urls))
 
-
-    #
-    # @slash_command(description="Choose between a selection of options.")
-    # async def choose(self, inter: SlashInteraction,
-    #                  choices: str = OptionParam(desc="List the different options with spaces between choices")):
-    #     random_selection = choice(choices.split(" "))
-    #     possible_choices = choices.split(" ")
-    #     possible_choices = str.join(", ", ["`" + my_choice + "`" for my_choice in possible_choices])
-    #     await inter.respond(f"**Possible Choices**: {possible_choices}\n" + f"**Selection**: `{random_selection}`")
     #
     # @message_command(name="Display Emoji")
     # async def displayemoji_app(self, inter: ContextMenuInteraction):
@@ -113,45 +114,7 @@ class MiscellaneousCog(commands.Cog):
     #     buttons = await get_bot_info_buttons(bot)
     #     await inter.respond(embed=embed, components=buttons)
     #
-    # @slash_command(description="Test command")
-    # async def test_pages(self, inter: SlashInteraction, num_pages: int):
-    #     embeds = []
-    #     for i in range(0, num_pages):
-    #         embeds.append(await create_bot_author_embed(self.bot.keys, title=f"test{i + 1}"))
     #
-    #     await create_paginated_embed(inter, embeds)
-    #
-    # @slash_command(description="Test command")
-    # async def test_menu(self, inter: SlashInteraction):
-    #     menu = [
-    #         SelectMenu(
-    #             custom_id="test",
-    #             placeholder="Choose up to 2 options",
-    #             max_values=2,
-    #             options=[
-    #                 SelectOption("Option 1", "value 1"),
-    #                 SelectOption("Option 2", "value 2"),
-    #                 SelectOption("Option 3", "value 3")
-    #             ]
-    #         )
-    #     ]
-    #     msg = await inter.reply("This message has a select menu!", components=menu)
-    #
-    #     def check(menu_inter):
-    #         nonlocal inter
-    #         return menu_inter.author == inter.author
-    #
-    #     menu_inter = await msg.wait_for_dropdown(check)
-    #     labels = [option.label for option in menu_inter.select_menu.selected_options]
-    #     await menu_inter.reply(f"Your choices: {', '.join(labels)}")
-    #
-    # @user_command(name="Created at")
-    # async def created_at(self, inter: ContextMenuInteraction):
-    #     # User commands always have only this ^ argument
-    #     await inter.respond(
-    #         f"{inter.user} was created at {inter.user.created_at}",
-    #         ephemeral=True # Make the message visible only to the author
-    #     )
 
 
 def setup(bot: commands.AutoShardedBot):
--- cogs/profile/ProfileCog.py
+++ cogs/profile/ProfileCog.py
@@ -1,17 +1,59 @@
+import disnake
 from disnake.ext import commands
+from disnake import ApplicationCommandInteraction as AppCmdInter
 
 
 class ProfileCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # @slash_command(description="Display user avatar")
-    # async def avatar(self, inter: SlashInteraction,
-    #                  user: disMember = OptionParam(lambda inter: inter.author)):
-    #     embed = await create_bot_author_embed(self.bot.keys, title=f"{user.display_name}'s Avatar ({user.id})")
-    #     embed.set_image(url=user.avatar_url)
-    #     await inter.respond(embed=embed)
-    #
+    # AvatarSource = commands.option_enum({"Global": "global", "Server": "server", "Both": "both"})
+    @commands.slash_command(description="Display user avatar")
+    async def avatar(
+        self,
+        inter: AppCmdInter,
+        user: disnake.Member = commands.Param(
+            lambda inter: inter.author, description="Member to display the avatars of."
+        ),
+    ):
+        avatar_embed = disnake.Embed(
+            title=f"{user.display_name}'s Avatar ({user.id})", url=user.avatar.url
+        )
+        avatar_embed.set_image(url=user.avatar.url)
+        if user.guild_avatar:
+            guild_avatar_embed = disnake.Embed(
+                title=f"{user.display_name}'s Avatar ({user.id})",
+                url=user.guild_avatar.url,
+            )
+            guild_avatar_embed.set_image(url=user.guild_avatar.url)
+            avatar_embeds = [avatar_embed, guild_avatar_embed]
+            return await inter.response.send_message(embeds=avatar_embeds)
+        await inter.response.send_message(embed=avatar_embed)
+        # TODO: Add footer and add source option parameter
+        # source: AvatarSource = commands.Param("both", description="User avatar sources.")
+
+    @commands.slash_command(description="Display user profile banner.")
+    async def banner(
+        self,
+        inter: AppCmdInter,
+        user: disnake.Member = commands.Param(
+            lambda inter: inter.author, description="Member to display the banner of."
+        ),
+    ):
+        # Due to API limitations, you must fetch the user from the client to get an object which contains the banner attribute
+        user_object = await self.bot.fetch_user(user.id)
+        if not user_object.banner:
+            return await inter.response.send_message(
+                f"{user.display_name} ({user.id}) does not have a profile banner.",
+                ephemeral=True,
+            )
+        banner_embed = disnake.Embed(
+            title=f"{user.display_name}'s Profile Banner ({user.id})",
+            url=user_object.banner.url,
+        )
+        banner_embed.set_image(url=user_object.banner.url)
+        await inter.response.send_message(embed=banner_embed)
+
     # @slash_command(description="View a user's profile information.")
     # async def profile(self, inter: SlashInteraction,
     #                   user: disMember = OptionParam(lambda inter: inter.author)):

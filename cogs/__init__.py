4c16ebf04fd9b657dc4af6141fa310837071e46b
MujyKun <mujy@irenebot.com>
2025-06-08 13:52:18 -0400

    Removed reminders'

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,18 +1,11 @@
 cogs_list = [
     "botowner.BotOwnerCog",
-    # "biasgame.BiasGameCog",
-    # "datamod.DataModCog",
-    # "groupmembers.GroupMembersCog",
     "miscellaneous.MiscellaneousCog",
     "profile.ProfileCog",
     "wolfram.WolframCog",
-    # "guessinggame.GuessingGameCog",
-    # "unscramblegame.UnscrambleGameCog",
     "twitch.TwitchCog",
     "moderator.ModeratorCog",
     "blockingmonitor.BlockingMonitorCog",
     "usernotifications.UserNotificationsCog",
     "interactions.InteractionsCog",
-    "reminders.RemindersCog",
-    # "tiktok.TikTokCog",
 ]
--- models/__init__.py
+++ models/__init__.py
@@ -3,29 +3,6 @@ from datetime import datetime
 from concurrent import futures
 
 
-def add_to_cache(obj):
-    if isinstance(obj, GroupGuessingGame):
-        gggs.append(obj)
-    elif isinstance(obj, GuessingGame):
-        ggs.append(obj)
-    elif isinstance(obj, UnscrambleGame):
-        uss.append(obj)
-    elif isinstance(obj, BiasGame):
-        bgs.append(obj)
-    else:
-        others.append(obj)
-    all_games.append(obj)
-
-
-ggs = []  # GuessingGame
-gggs = []  # GroupGuessingGame
-uss = []  # UnscrambleGame
-bgs = []  # BiasGame
-bjs = []  # BlackJack
-others = []  # etc
-all_games = []
-
-
 # groupmembers search
 distance_between_words: Dict[str, Dict[str, float]] = dict()
 requests_today = 0
@@ -46,16 +23,6 @@ from .StatsTracker import StatsTracker, Trackable
 from dataclasses import dataclass
 from IreneAPIWrapper.models import User, UserStatus
 import disnake
-
-
-@dataclass
-class PlayerScore(UserStatus):
-    user: User
-    disnake_user: disnake.User
-    status: UserStatus
-
-    def __str__(self):
-        return f"{self.disnake_user.display_name} -> {self.status.score}."
 
 
 from .CommandTypes import (
